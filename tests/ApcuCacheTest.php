0664bac11a11af36db55fea3663fcbe18c9980ea
Alexander Makarov <sam@rmcreative.ru>
2019-11-30 00:46:26 +0000

    Apply fixes from StyleCI

--- tests/ApcuCacheTest.php
+++ tests/ApcuCacheTest.php
@@ -287,8 +287,7 @@ class ApcuCacheTest extends TestCase
             ],
             'IteratorAggregate' => [
                 ['a' => 1, 'b' => 2,],
-                new class() implements \IteratorAggregate
-                {
+                new class() implements \IteratorAggregate {
                     public function getIterator()
                     {
                         return new \ArrayIterator(['a' => 1, 'b' => 2,]);
